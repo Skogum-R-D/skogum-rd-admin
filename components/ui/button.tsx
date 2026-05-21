73eb5f3e832e0c8f565d9858b55de99e8b31bc00
Simen Andre Vikestrand Skogum <v0nNemizez@gmail.com>
2026-05-21 06:28:14 +0200

    chore: add button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -34,7 +34,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
       transition={{ type: "spring", stiffness: 400, damping: 17 }}
-      {...props}
+      {...(props as any)}
     />
   )
 );
